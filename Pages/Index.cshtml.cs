7385055926a8fd80f945920aa59f3c8a47783df0
Gilbert <gssenyonjo@gmail.com>
2022-10-15 14:55:59 +0300

    Finished mvp implementation for shortening urls

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ShortenUrl.Data;
+using ShortenUrl.Models;
 
 namespace ShortenUrl.Pages;
 
@@ -19,19 +20,23 @@ public class IndexModel : PageModel
     public string ErrorMessage;
 
     [BindProperty]
-    public string OriginalUrl { get; set; } = "";
+    public ShortenedUrl ShortenedUrl { get; set; }
 
-    public string ShortUrl { get; set; } = "";
+    public string FullShortUrl { get; set; }
 
 
-    public void OnGet()
+    public ActionResult OnPost()
     {
-
-    }
-
-    public ActionResult OnPost() 
-    {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-        ShortUrl = $"Shortened {OriginalUrl}";
+        ShortenedUrl.ShortUrl = randomShortUrl();
+        _context.ShortenedUrl.Add(ShortenedUrl);
+        _context.SaveChanges();
+        FullShortUrl = $"{HttpContext.Request.Host}{ShortenedUrl.ShortUrl}";
         return Page();
     }
+
+    private string randomShortUrl()
+    {
+        return "/sh/" + Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 5).ToLower();
+    }
 }
+
---
+++ Pages/sh.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShortenUrl.Data;
+using ShortenUrl.Models;
+
+namespace ShortenUrl.Pages;
+
+public class ShModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly ShortenContext _context;
+
+    public ShModel(ILogger<IndexModel> logger, ShortenContext shortenContext)
+    {
+        _logger = logger;
+        _context = shortenContext;
+    }
+
+
+    [BindProperty]
+    public ShortenedUrl ShortenedUrl { get; set; }
+
+    public string FullShortUrl { get; set; }
+
+    public ActionResult OnGet()
+    {
+        _logger.LogInformation($"url path is {HttpContext.Request.Path}");
+
+        ShortenedUrl = _context.ShortenedUrl
+            .Where(s => s.ShortUrl == HttpContext.Request.Path.ToString())
+            .FirstOrDefault();
+        if (ShortenedUrl != null)
+            return Redirect(ShortenedUrl.OriginalUrl);
+        return RedirectToPage("./Index");
+    }
+}
+
+
